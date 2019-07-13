b64252dda3bc10830b9fbb3d55e58ca7cacfc754
Anabel Perez <anabelpdv@gmail.com>
2019-07-12 23:17:41 -0400

    Add shapes and colors

--- games/tetris/tetris.js
+++ games/tetris/tetris.js
@@ -1,12 +1,16 @@
 let canvas = document.querySelector('#tetris');
 let context = canvas.getContext('2d');
-canvas.width =480;
-canvas.height = 800;
+canvas.width =468;
+canvas.height = 780;
+
+window.onload =()=>{
+
+  let audio = new Audio("Popcorn.wav");
+  audio.play();
+}
 
 
-
-context.scale(40,40);//make the components bigger
-
+context.scale(39,39);//make the components bigger
 
 
 const matrix = [
@@ -26,6 +30,15 @@ const matrix = [
 /***************************************************************************************************************** */
 /***************************************************************************************************************** */
 
+
+function arenaSweep(){
+  for(let y = arena.length - 1; y >= 0; y--){
+      if(arena[y].every(x => x !== 0)){
+       arena.unshift(arena.splice(y,1)[0].fill(0));
+        y++;
+      }
+   }
+ }
 
 function collide(arena,player){
   [m, o] = [player.matrix, player.pos];
@@ -49,23 +62,74 @@ function createMatrix(w,h){
   return matrix;
 }
 
+function createPiece(type){
+  switch(type){
+    case 'T':
+      return[
+      [0,0,0],
+      [1,1,1],
+      [0,1,0]
+    ];
+   case 'O':
+      return[
+      [2,2],
+      [2,2],
+    ];
+   case 'I':
+      return[
+      [0,3,0,0],
+      [0,3,0,0],
+      [0,3,0,0],
+      [0,3,0,0],
+    ];
+   case 'S':
+      return[
+      [0,4,4],
+      [4,4,0],
+      [0,0,0]
+    ];
+  case 'Z':
+      return[
+      [5,5,0],
+      [0,5,5],
+      [0,0,0]
+    ];
+  case 'J':
+      return[
+      [6,0,0],
+      [6,6,6],
+      [0,0,0]
+    ];
+  case 'L':
+      return[
+      [0,0,7],
+      [7,7,7],
+      [0,0,0]
+    ];
+
+  }
+  
+
+}
+
 function draw(){
-  //context.fillStyle = "#000";
   context.clearRect(0,0,canvas.width,canvas.height);
   drawMatrix(arena, {x:0, y:0})
   drawMatrix(player.matrix,player.pos);
 }
 
+const colors = ['null','#D26414', 'blue', 'purple', 'yellow','green','red','gray']
 function drawMatrix(matrix, offset){
   matrix.forEach((row,y)=>{
     row.forEach((value,x)=>{
-      if(value !==0){
-        context.fillStyle = 'blue';
+      if(value !== 0){
+       context.fillStyle = colors[value];
         context.fillRect(x + offset.x, y + offset.y, 1, 1);
       }
     });
   });
 }
+
 
 function merge(arena, player){
   player.matrix.forEach((row,y)=>{
@@ -81,8 +145,9 @@ function playerDrop(){
   player.pos.y++;
   if(collide(arena, player)){
     player.pos.y--;
-    merge(arena, player);
-    player.pos.y = 0;
+    merge(arena, player);/***************this might be the place where to change the player****************/ 
+    playerReset();
+    arenaSweep();
   }
   dropCounter = 0;
 }
@@ -90,22 +155,24 @@ function playerDrop(){
 let dropCounter = 0;
 let dropInterval = 1000;
 let lastTime = 0;
-function update(time=0){
-  const deltaTime = time - lastTime;
+
+function update(time = 0){// time is an argument that the callback function of requestAnimationFrame receives automatically like the event argument
+  
+  const deltaTime = time - lastTime;//time interval in milliseconds between frames
   lastTime = time;
+  dropCounter += deltaTime;//this eventually adds up to a number greater than the drop inteval and as soon as that is true another player is moved down 1px;
 
-
-  dropCounter += deltaTime;
   if(dropCounter > dropInterval){
-    playerDrop();
+    playerDrop();//move player 1 px
   }
-  draw();
+  draw();//draw player in new position and arena too
   requestAnimationFrame(update)
 }
 
+
 const player = {
   pos:{x:5, y:0,},
-  matrix: matrix, 
+  matrix: createPiece('T')
 }
 const arena = createMatrix(12,20);
 
@@ -115,6 +182,16 @@ function playerMove(dir){
     player.pos.x -= dir;
   }
 }
+
+function playerReset(){
+  const pieces = 'TOISZJL';
+  player.matrix= createPiece(pieces[pieces.length * Math.random() | 0]);
+  player.pos.y = 0;
+  player.pos.x = (arena.length / 2 | 0) - (player.matrix[0].length/2 |0)
+}
+
+
+
 function playerRotate(dir){
   const pos = player.pos.x;
   let offset = 1;
